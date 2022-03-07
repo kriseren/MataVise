d6b2f43b52577c16d74e601548fe91cc4bf1ee38
kriseren <77700523+kriseren@users.noreply.github.com>
2022-03-07 10:19:46 +0100

    Add files via upload

--- Sounds.java
+++ Sounds.java
@@ -10,14 +10,14 @@ class Sounds {
    public static void bros() throws FileNotFoundException,JavaLayerException
    {
 
-      Player apl = new Player(new FileInputStream(".sounds/Bros.mp3"));
+      Player apl = new Player(new FileInputStream("sounds/Bros.mp3"));
       apl.play();
    }
 
    public static void coin() throws FileNotFoundException,JavaLayerException
    {
 
-      Player apl = new Player(new FileInputStream(".sounds/coin.mp3"));
+      Player apl = new Player(new FileInputStream("sounds/coin.mp3"));
       apl.play();
    }
 }
