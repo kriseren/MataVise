d1243f399f175faf242f1cfe9a0d5fd453f25ba1
Diego <77700523+kriseren@users.noreply.github.com>
2022-03-20 18:34:34 +0100

    Add files via upload

---
+++ mataVise.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+clear && javac -d bin/ -cp jar/jlayer-1.0.1.jar src/mataVise/*.java
+mate-terminal --zoom=1.8 --full-screen --command "java -cp jar/jlayer-1.0.1.jar:bin/ mataVise.mataVise" --display=:0
